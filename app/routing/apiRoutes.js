c6fc7641ff7c23b9aa7a8da2a6673a14cb55a964
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-10 14:06:55 -0700

    initial routing is working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,9 @@
-app.get("/api/friends", function (req, res) {
-    res.send(data.json)
-});
+module.exports = function (app, path) {
+    app.get("/api/friends", function (req, res) {
+        res.send(data.json)
+    });
 
-app.post("/api/friends", function (req, res) {
-    //Add the shit
-});
+    app.post("/api/friends", function (req, res) {
+        //Add the shit
+    });
+};
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 var PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+require("./app/routing/htmlRoutes")(app, path);
+require("./app/routing/apiRoutes")(app, path);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
