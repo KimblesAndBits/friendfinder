4244bf0a3ca9ee41650fcd24341eb97597d1ef9b
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-11 18:41:52 -0700

    quick port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require("./app/routing/htmlRoutes")(app, path);
