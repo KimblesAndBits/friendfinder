2fdff1db6c2c785670f4603fcbd9d26b10e2025d
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-11 18:23:42 -0700

    working app, need more css

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,6 @@ var friends = require("./../data/friends").friends.friends;
 module.exports = (app) => {
     app.get("/api/friends", (req, res) => {
         var you = friends[friends.length - 1];
-        console.log(you);
         var match;
         var matchNumber = 40;
         friends.forEach(element => {
@@ -25,7 +24,34 @@ module.exports = (app) => {
         };
         friends.push(newFriend);
         console.log("Succefully added a new friend");
-        console.log(friends);
         return res.json("false");
+    });
+
+    app.get("/api/friends/:name", (req, res) => {
+        var you;
+        friends.forEach(element => {
+            if (req.params.name === element.name) {
+                you = element;
+            };
+        });
+        if (typeof you === 'undefined') {
+            you = {
+                name: "No one",
+                password: "nothing"
+            };
+        };
+        var match = [you];
+        var matchNumber = 40;
+        friends.forEach(element => {
+            var thisMatch = 0;
+            for (var i =0; i < element.scores.length; i++) {
+                thisMatch += Math.abs(element.scores[i] - you.scores[i]);
+            };
+            if (thisMatch < matchNumber && element.name !== you.name) {
+                matchNumber = thisMatch;
+                match.push(element);
+            };
+        });
+        return res.json(match);
     });
 };
